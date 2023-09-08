b10fe7440473c32bdd7439e7585cadaa0f65fcab
Dawid <dembinski.dawid@gmail.com>
2023-09-08 14:33:31 +0200

    Add failing test

--- src/test/java/com/dembinski/ufcapi/UfcApiApplicationTests.java
+++ src/test/java/com/dembinski/ufcapi/UfcApiApplicationTests.java
@@ -3,11 +3,18 @@ package com.dembinski.ufcapi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class UfcApiApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void failTest() {
+		assertTrue(1 == 0);
 	}
 
 }
