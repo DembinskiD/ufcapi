23a2620b6cffb571524a5d873c21f0b1dcb7fc19
Dawid <dembinski.dawid@gmail.com>
2023-10-02 15:44:13 +0200

    Finish FightReader downloading/reading file flow

--- src/main/java/com/dembinski/ufcapi/json/FightReader.java
+++ src/main/java/com/dembinski/ufcapi/json/FightReader.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -17,15 +19,10 @@ import java.util.Objects;
 @Service
 @Slf4j
 public class FightReader {
-    private final List<Fight> allFights;
-    private final String apiKey = "5a881fececbfe0a958072272a50be4ea6d67d534";
-    private final String uri = String.format("https://fightingtomatoes.com/API/%s/Any/Any/Any", apiKey);
-    private final int updateEveryDays = 10;
-    private String TARGET_FILE_NAME = "full_data.json";
-
-    public FightReader() {
-        this.allFights = generateFightList();
-    }
+    private final String apiKey = "5a881fececbfe0a958072272a50be4ea6d67d534"; //todo move it to properties
+    private final String uri = String.format("https://fightingtomatoes.com/API/%s/Any/Any/Any", apiKey); //todo move it to properties
+    private final int updateEveryDays = 10; //todo move it to properties
+    private String TARGET_FILE_NAME = "full_data.json"; //todo as above
 
     private FightList readFightListFromFile() {
         try {
@@ -40,6 +37,14 @@ public class FightReader {
         }
     }
 
+    private void removeExistingFightList() {
+        try {
+            Files.delete(Path.of(TARGET_FILE_NAME));
+        } catch (IOException e) {
+            log.debug("Issue removing existing file {}", TARGET_FILE_NAME);
+        }
+    }
+
     protected void saveToFile(FightList fightList) {
         try {
             FileOutputStream fileOutputStream = new FileOutputStream(TARGET_FILE_NAME);
@@ -50,11 +55,22 @@ public class FightReader {
         }
     }
 
-
-    private List<Fight> generateFightList() {
-        FightList existingData = readFightListFromFile();
-
-        return new ArrayList<>(); //todo continue check if file exist, if should be updated and eventually update here[
+    public List<Fight> getAllFights() {
+        if (Files.exists(Path.of(TARGET_FILE_NAME))) {
+            FightList fightList = readFightListFromFile();
+            if (shouldUpdateData(fightList)) {
+                removeExistingFightList();
+                FightList fightListFromAPI = getFightListFromAPI();
+                saveToFile(fightListFromAPI);
+                return fightListFromAPI.fights();
+            } else {
+                return fightList.fights();
+            }
+        } else {
+            FightList fightListFromAPI = getFightListFromAPI();
+            saveToFile(fightListFromAPI);
+            return fightListFromAPI.fights();
+        }
     }
 
     private FightList getFightListFromAPI() {
@@ -62,8 +78,8 @@ public class FightReader {
         ObjectMapper mapper = new ObjectMapper();
         List<Fight> list;
 
-        String stringData = Objects.requireNonNull(restTemplate.getForObject(uri, String.class)).
-                lines()
+        String stringData = Objects.requireNonNull(restTemplate.getForObject(uri, String.class))
+                .lines()
                 .filter(line -> line.startsWith("[") && line.endsWith("]"))
                 .findFirst()
                 .orElse("");
@@ -74,13 +90,10 @@ public class FightReader {
             list = new ArrayList<>();
         }
 
-        FightList fightList = createListOfFights(list);
-        saveToFile(fightList);
-        return fightList;
-
+        return createFightList(list);
     }
 
-    private FightList createListOfFights(List<Fight> fightList) {
+    private FightList createFightList(List<Fight> fightList) {
         return FightList
                 .builder()
                 .fights(fightList)
@@ -92,9 +105,5 @@ public class FightReader {
         int daysSinceLastUpdate = fightList.createdAt().until(LocalDate.now()).isNegative() ?
                 LocalDate.now().until(fightList.createdAt()).getDays() : fightList.createdAt().until(LocalDate.now()).getDays();
         return daysSinceLastUpdate > updateEveryDays;
-    }
-
-    public List<Fight> getAllFights() {
-        return this.allFights;
     }
 }
